4f5fa4d5f5559f9e90c5a7b4d3cede7740f04d57
Sven <67136317+Sven-Bo@users.noreply.github.com>
2022-08-14 15:56:55 +0200

    Adjust setup.sh file

--- setup.sh
+++ setup.sh
@@ -1,1 +1,12 @@
-[server]\nheadless = true\nenableCORS=false\nport = \n
+mkdir -p ~/.streamlit/
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+[theme]\n\
+primaryColor = '#E694FF'\n\
+backgroundColor = '#00172B'\n\
+secondaryBackgroundColor = '#0083B8'\n\
+" > ~/.streamlit/config.toml
